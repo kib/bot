512172197884f65dcd04163ea343c37ff9ffdf81
Jasper Capel <jasper@capel.tv>
2013-04-02 19:18:40 +0200

    Added endless scrolling to image viewer

--- src/viewer/views/images.py
+++ src/viewer/views/images.py
@@ -1,14 +1,17 @@
-from django.http import HttpResponse
-from django.template import Context, loader
+from django.template import Context, RequestContext
 from viewer.utils import context
 from viewer.models import Image
+from endless_pagination.decorators import page_template
+from django.shortcuts import render_to_response
 
 
-def images(request, channel=None, nsfw=None):
-    template = loader.get_template('viewer/images.html')
+@page_template('viewer/images/page.html')
+def images(request, channel=None, nsfw=None, template='viewer/images/index.html', extra_context=None):
     images = Image.objects.order_by('-posted_time')
     #TODO: this could get expensive with large amounts of images
     channels = set([x.posted_channel for x in Image.objects.all()])
     c = context(request)
     c.update({'images': images, 'channels': channels})
-    return HttpResponse(template.render(c))
+    if extra_context is not None:
+        c.update(extra_context)
+    return render_to_response(template, c, context_instance=RequestContext(request))

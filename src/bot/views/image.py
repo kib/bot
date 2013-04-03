b0e06121d6df082b78fbdf8d304e51582549e4fc
Jasper Capel <jasper@capel.tv>
2013-04-03 21:40:50 +0200

    improved nsfw detection

--- src/bot/views/image.py
+++ src/bot/views/image.py
@@ -5,6 +5,6 @@ from bot.utils import parse_request, fix_url
 def image(request, url=None):
     url = fix_url(url)
     (c, n, m) = parse_request(request)
-    nsfw = True if 'nsfw' in m else False
+    nsfw = True if 'nsfw' in m.lower() else False
     img_obj = Image.objects.create(dest_url=url, nsfw=nsfw, posted_channel=c, posted_by=n, message=m)
     return IRCResponse('#test', 'added')

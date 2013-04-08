643669636d377889de28b1251ad42e86a440d2a2
Jasper Capel <jasper@capel.tv>
2013-04-08 21:10:07 +0200

    Better ImageAdmin

--- src/viewer/admin.py
+++ src/viewer/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from viewer.models import *
 
-admin.site.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    date_hierarchy = 'posted_time'
+    list_display = ('dest_url', 'posted_channel', 'posted_time', 'posted_by', 'nsfw')
+
+admin.site.register(Image, ImageAdmin)
 admin.site.register(Link)
 admin.site.register(Video)

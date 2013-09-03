a899bccec139e2a4d9e24abb476dbe50b96175f4
Jasper Capel <jasper.capel@spilgames.com>
2013-09-03 08:51:41 +0200

    added civ5

--- src/bot/views/__init__.py
+++ src/bot/views/__init__.py
@@ -1,6 +1,7 @@
 from yardbird.irc import IRCResponse
 from viewer.models import Image, Link
 from bot.utils import parse_request
+from civ5 import *
 from image import *
 from link import *
 from hoi import *
---
+++ src/bot/views/civ5.py
@@ -0,0 +1,19 @@
+from yardbird.irc import IRCResponse
+from viewer.models import Image, Link
+from bot.utils import parse_request
+from bs4 import BeautifulSoup
+import requests
+
+def civ5(request):
+    d = request.reply_recipient
+    game_url = 'http://multiplayerrobot.com/Game/Details?id=5152'
+    data = requests.post(game_url, headers={'Content-Length': '0'})
+    if data.status_code != 200:
+        return IRCResponse(d, 'Unable to fetch game data')
+
+    soup = BeautifulSoup(data.text)
+
+    active_player = soup.find(class_='game-host').find(class_='avatar').attrs['title']
+    turn_timer = soup.find(id='turn-timer-container').find('strong').string
+
+    return IRCResponse(d, "Active player: {0}, turn ends: {1}".format(active_player, turn_timer))

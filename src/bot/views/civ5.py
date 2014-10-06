7587d1037750c3eeb4e5baf72625ff44002e6060
Kib <kib@users.noreply.github.com>
2014-10-06 21:16:58 +0200

    Change multiplayerrobot URL
    Since they are redirecting to it from the URL we were using previously.

--- src/bot/views/civ5.py
+++ src/bot/views/civ5.py
@@ -7,7 +7,7 @@ import requests
 def civ5(request):
     d = request.reply_recipient
     game_ids = ['10774','10775','14878']
-    base_url = 'http://multiplayerrobot.com/Game/Details?id='
+    base_url = 'http://multiplayerrobot.com/Game#'
     response = ""
     for i in game_ids:
         data = requests.post(base_url + i, headers={'Content-Length': '0'})

ce0547e9b4884e89848debca1de768075f0912b0
Ejal de Klerk <kibje@spores.kibje.com>
2014-12-01 10:50:03 +0100

    Remove game #3 So the bot works again.

--- src/bot/views/civ5.py
+++ src/bot/views/civ5.py
@@ -6,7 +6,7 @@ import requests
 
 def civ5(request):
     d = request.reply_recipient
-    game_ids = ['10774','10775','14878']
+    game_ids = ['10774','10775']
     base_url = 'http://multiplayerrobot.com/Game#'
     response = ""
     for i in game_ids:
